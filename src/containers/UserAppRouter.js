5e5486e04db93f342e7a3ca74e00eb56eedf8def
Aditya Shridhar Hegde <adishegde@gmail.com>
2018-03-18 19:53:24 +0530

    Refactor UserAppRouter container

--- src/containers/UserAppRouter.js
+++ src/containers/UserAppRouter.js
@@ -2,9 +2,28 @@
  * depending on the state of authentication by using the RequireSignIn
  * component. If signed in it renders the UserAppRouter component */
 
+import React from "react";
 import { connect } from "react-redux";
 import { isAuthenticating, isSignedIn } from "selectors/index";
-import UserAppRouter from "components/UserAppRouter/index";
+import { Dimmer, Loader } from "semantic-ui-react";
+import { Redirect } from "react-router-dom";
+import UserAppRouter from "components/UserAppRouter";
+
+function RouteIfSignedIn({ isAuthenticating, isSignedIn }) {
+    if (isAuthenticating) {
+        // Display loading bar if authenticating
+        return (
+            <Dimmer active>
+                <Loader size="huge"> Configuring App. Please Wait. </Loader>
+            </Dimmer>
+        );
+    } else if (isSignedIn) {
+        return <UserAppRouter />;
+    } else {
+        // Redirect to login page if not signed in
+        return <Redirect to="/login" />;
+    }
+}
 
 function mapStateToProps(state) {
     return {
@@ -13,4 +32,4 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps)(UserAppRouter);
+export default connect(mapStateToProps)(RouteIfSignedIn);
